8dbf2788447de7831af6ff2f42329af98d0f81c1
Shelem Oteo <shembot@Shelems-MacBook-Air.local>
2023-05-22 19:39:24 -0600

    trouble shooting

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import dummyContacts from '../dummyData/index.js';
+
 
 const Main = () => {
+  const [contacts, setContacts] = useState(dummyContacts);
+
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
       </div>
-      <div id="container">{/* your components here */}</div>
+      <div id="container">
+        {
+        contacts.map(contact => {
+          return contact.name;
+        }) }</div>
     </div>
   );
 };
